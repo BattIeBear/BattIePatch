8e61e24ea16edbbeaa6f72e44a1ce122f89bb9db
BattIeBear <69400780+BattIeBear@users.noreply.github.com>
2025-06-24 16:47:34 -0400

    Accidently uploaded all of rimworld.

---
+++ BattIePatch - Ghoul Mood Bar Shows Their Hunger/1.5/Source/BattIePatch - Ghoul Mood Bar Shows Their Hunger/Utils/FoodToMood.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BattIePatch_GhoulMoodBarFix
+{
+    public static class FoodToMood
+    {
+        public static MoodThreshold Convert(Pawn pawn)
+        {
+            if (pawn.needs.food.CurCategory == HungerCategory.UrgentlyHungry || pawn.needs.food.CurCategory == HungerCategory.Starving)
+            {
+                return MoodThreshold.Extreme;
+            }
+            else if (pawn.needs.food.CurLevelPercentage <= .25f)
+            {
+                return MoodThreshold.Major;
+            }
+            else if (pawn.needs.food.CurLevelPercentage <= .5f)
+            {
+                return MoodThreshold.Minor;
+            }
+
+            return MoodThreshold.None;
+        }
+    }
+}
